b577a81f3e88759136fc54a2e50ddce4fd1780fe
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2022-12-18 11:44:07 +0100

    hola
    cambios2

--- ej1.py
+++ ej1.py
@@ -1,9 +1,17 @@
-def hello():
-    print ("Hello World!")
-    return
+def onperacion():
+    num1 = int(input("Ingrese el primer numero: "))
+    num2 = int(input("Ingrese el segundo numero: "))
+    try:
+        operacion = num1/num2
+    except ZeroDivisionError:
+        print("No se puede dividir por 0")
+    return operacion
 
-
+def evaluar_lista():
+    lista = [1,2,3,4,5,6,7,8,9,10]
+    print(lista[14])
+    
 
 
 if __name__ == "__main__":
-    hello()
+    print(evaluar_lista())
---
+++ ej2.py
@@ -0,0 +1,8 @@
+def evaluar_lista():
+    lista = [1,2,3,4,5,6,7,8,9,10]
+    print(lista[14])
+    
+
+
+if __name__ == "__main__":
+    print(evaluar_lista())
