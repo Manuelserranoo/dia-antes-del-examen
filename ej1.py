4af5fd7840b86cd62441cfd1fe5e4ec9d1dad459
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2022-12-18 11:45:23 +0100

    Update ej1.py
    cambios

--- ej1.py
+++ ej1.py
@@ -7,11 +7,8 @@ def onperacion():
         print("No se puede dividir por 0")
     return operacion
 
-def evaluar_lista():
-    lista = [1,2,3,4,5,6,7,8,9,10]
-    print(lista[14])
-    
+
 
 
 if __name__ == "__main__":
-    print(evaluar_lista())
+    print(onperacion())
