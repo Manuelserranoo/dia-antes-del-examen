95494923375e65cc0cf127b289a4dd9ee0fd88a0
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2022-12-19 01:19:13 +0100

    cambios
    cambios de dormir

--- exámen/ej2.py
+++ exámen/ej2.py
@@ -4,12 +4,12 @@ a =  palabra.replace(" ","")
 def crear_matriz(m,n):
     for j in range(n):
         return [[0 for j in range(n)] for i in range(m)]
-    for k in j:
-        if k == a[0]:
-            matriz.append(k)
+
 
 filas = int(input("Ingrese la clave: "))
 columnas  = len(a)
+def intruducir_texto():
+    for palabra in 
 if __name__ == "__main__":
     matriz = crear_matriz(filas,columnas)
     print(matriz)
